ab3457ef1bf5dec8d9c94606ae0adbc527729b95
bebtio <bebtio@email.arizona.edu>
2016-02-04 12:48:56 -0700

    summer

---
+++ build/src/CMakeFiles/huffman.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/huffman.dir/huffmanencoder.cpp.o"
+  "CMakeFiles/huffman.dir/main.cpp.o"
+  "huffman.pdb"
+  "huffman"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/huffman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
